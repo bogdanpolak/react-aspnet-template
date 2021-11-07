e5bdbbaa88f9488e45f6b5a1cf2702981d70eab2
Bogdan Polak <bogdanpol@gmail.com>
2021-11-07 16:40:00 +0100

    Added compoment IncrementEditor

--- WebAppReact/ClientApp/src/components/Counter.js
+++ WebAppReact/ClientApp/src/components/Counter.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
+import { IncrementEditor } from "./IncrementEditor";
 
 export class Counter extends Component {
   static displayName = Counter.name;
 
   constructor(props) {
     super(props);
-    this.state = { currentCount: 0 };
+    this.state = { 
+      currentCount: 0,
+      currentIncrement: 1, 
+    };
     this.incrementCounter = this.incrementCounter.bind(this);
+    this.onIncrementChange = this.onIncrementChange.bind(this);
   }
 
   incrementCounter() {
-    this.setState({
-      currentCount: this.state.currentCount + 1
+    this.setState((state) => ({
+      currentCount: state.currentCount + state.currentIncrement
+    }));
+  }
+
+  onIncrementChange(value) {
+    this.setState({ 
+      currentIncrement: value 
     });
   }
 
@@ -24,7 +35,11 @@ export class Counter extends Component {
 
         <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
 
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
+        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment by {this.state.currentIncrement}</button>
+          
+        <div className="mt-5">
+          <IncrementEditor onIncrementChange={this.onIncrementChange} />
+        </div>  
       </div>
     );
   }
---
+++ WebAppReact/ClientApp/src/components/IncrementEditor.js
@@ -0,0 +1,29 @@
+﻿import React from 'react';
+
+function IncrementEditor(props) {
+  const [increment, setIncrement] = React.useState(1);
+
+  const onEditIncrement = (e) => {
+    const increment = parseInt(e.target.value);
+    if (isNaN(increment)) return;
+    changeIncrement(increment);
+  }
+
+  const changeIncrement = (value) => {
+    props.onIncrementChange(value)
+    setIncrement(value)
+  }
+
+  return (
+    <div>
+      <p aria-live="polite" className="mb-1">Increment Editor</p>
+
+      <input value={increment} className="input-lg" onChange={onEditIncrement}/>
+
+      <button className="btn btn-primary ml-1" onClick={() => changeIncrement(increment - 1)}>-1</button>
+      <button className="btn btn-primary ml-1" onClick={() => changeIncrement(increment + 1)}>+1</button>
+    </div>
+  );
+}
+
+export {IncrementEditor};
